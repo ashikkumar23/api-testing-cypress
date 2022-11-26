bbeb82ec42efd3c64a6fe48685d9f8e38700b7f5
Ashik Kumar Billava Padmaiah <Ashik.BillavaPadmaiah@pfizer.com>
2022-11-26 12:28:57 +0530

    Initial changes

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const {
+    defineConfig
+} = require("cypress");
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: 'https://gorest.co.in',
+        setupNodeEvents(on, config) {
+            on('task', {
+                log(message) {
+                    console.log(message + '\n');
+                    return null;
+                    // Then to see the log messages in the terminal
+                    // Use: cy.task("log", "my message");
+                },
+            });
+        }
+    },
+});
