3422a9319a18f8de17264af65ae588f602c64068
Ashik Kumar Billava Padmaiah <Ashik.BillavaPadmaiah@pfizer.com>
2022-11-27 17:23:32 +0530

    Add GET and PUT route tests

--- cypress/e2e/test_crud.cy.js
+++ cypress/e2e/test_crud.cy.js
@@ -9,7 +9,7 @@ describe('Create a new user', () => {
                 method: 'POST',
                 url: '/public/v2/users',
                 headers: {
-                    'authorization': 'Bearer ' + Cypress.env('TOKEN'), // Generate access token from https://gorest.co.in/consumer/login and add the same to cypress.env.json
+                    'authorization': 'Bearer ' + Cypress.env('ACCESS_TOKEN'), // Generate access token from https://gorest.co.in/consumer/login and add the same to cypress.env.json
                 },
                 body: {
                     "name": userName,
@@ -29,6 +29,50 @@ describe('Create a new user', () => {
     })
 })
 
+describe('Get user details', () => {
+    context('GET /public/v2/users/${userId}', () => {
+        it('Test GET request', () => {
+            cy.request({
+                method: 'GET',
+                url: `/public/v2/users/${userId}`,
+                headers: {
+                    'authorization': 'Bearer ' + Cypress.env('ACCESS_TOKEN'), // Generate access token from https://gorest.co.in/consumer/login and add the same to cypress.env.json
+                }
+            }).then((response) => {
+                expect(response).to.have.property('status').to.equal(200)
+                expect(response.body).to.have.property('name').to.equal(userName)
+                expect(response.body).to.have.property('email').to.equal(emailId)
+                cy.task('log', 'Retrieved user with id: ' + userId)
+            })
+        })
+    })
+})
+
+describe('Update user details', () => {
+    context('PUT /public/v2/users/${userId}', () => {
+        it('Test PUT request', () => {
+            cy.request({
+                method: 'PUT',
+                url: `/public/v2/users/${userId}`,
+                headers: {
+                    'authorization': 'Bearer ' + Cypress.env('ACCESS_TOKEN'), // Generate access token from https://gorest.co.in/consumer/login and add the same to cypress.env.json
+                },
+                body: {
+                    "name": "Updated Name",
+                    "gender": "male",
+                    "email": "updatedemail@domain.com",
+                    "status": "active"
+                }
+            }).then((response) => {
+                expect(response).to.have.property('status').to.equal(200)
+                expect(response.body).to.have.property('name').to.equal("Updated Name")
+                expect(response.body).to.have.property('email').to.equal("updatedemail@domain.com")
+                cy.task('log', 'Updated user with id: ' + userId)
+            })
+        })
+    })
+})
+
 describe('Delete user', () => {
     context('DELETE /public/v2/users/${userId}', () => {
         it('Test DELETE request', () => {
@@ -36,7 +80,7 @@ describe('Delete user', () => {
                 method: 'DELETE',
                 url: `/public/v2/users/${userId}`,
                 headers: {
-                    'authorization': 'Bearer ' + Cypress.env('TOKEN'),
+                    'authorization': 'Bearer ' + Cypress.env('ACCESS_TOKEN'), // Generate access token from https://gorest.co.in/consumer/login and add the same to cypress.env.json
                 }
             }).then((response) => {
                 expect(response).to.have.property('status').to.equal(204)
